6f077812634d8c3e669166da8614a67e4408bbd4
Daniel-Anx <danielcoimbra.anx@gmail.com>
2025-06-05 13:05:02 -0300

    passando as informações certinhas, usando O banco de dados H2

--- src/main/java/br/com/fiap/aquamind/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/aquamind/config/SwaggerConfig.java
@@ -1,24 +1,31 @@
 package br.com.fiap.aquamind.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configurações básicas do OpenAPI/Swagger usando Springdoc-OpenAPI.
+ * Configurações do OpenAPI/Swagger para expor o botão de Authorize (Bearer JWT).
  *
  * Após rodar a aplicação, a documentação estará disponível em:
  *   - JSON:  /v3/api-docs
- *   - Swagger UI: /swagger-ui.html (ou /swagger-ui/index.html)
+ *   - Swagger UI: /swagger-ui/index.html (ou /swagger-ui/index.html#/)
  */
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        // Nome do esquema de segurança (irá aparecer no botão Authorize)
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+                // 1) Informação básica da API (titulo, versão, contato etc.)
                 .info(new Info()
                         .title("AquaMind API")
                         .version("v1")
@@ -28,6 +35,21 @@ public class SwaggerConfig {
                                 .email("suporte@aquamind.com.br")
                                 .url("https://www.aquamind.com.br")
                         )
-                );
+                )
+                // 2) Declaração do componente (Scheme) de segurança JWT Bearer
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)           // Tipo: HTTP
+                                        .scheme("bearer")                         // Esquema: bearer
+                                        .bearerFormat("JWT")                      // Formato: JWT
+                                        .in(SecurityScheme.In.HEADER)             // Onde passa o token: header
+                                        .description("Insira o JWT no formato: Bearer {token}")
+                        )
+                )
+                // 3) Exige que todas as operações usem o securitySchemeName ("bearerAuth")
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
     }
+
 }
